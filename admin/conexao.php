54d11e3c5d395d966f1e7b648484bf1934fcc36c
LUHAN BRITO BORGES <luhan.bborges@senacsp.edu.br>
2021-05-11 17:19:32 -0300

    atualizado

---
+++ admin/conexao.php
@@ -0,0 +1,12 @@
+<?php
+// conexão banco de dados mysql (Simples)
+   // $host = "localhost";
+   // $usuario = "root";
+   //$senha = "usbw";
+   //$banco = "dinamicodb";
+
+    $cn = new PDO("mysql:host=localhost;dbname=dinamicodb","root", "usbw");
+    //var_dump($cn);
+   //var_dump($db);
+
+?>
---
+++ admin/op_categoria.php
@@ -0,0 +1,19 @@
+<?php
+ include "conexao.php";
+
+ $txt_categoria = $_POST['txt_categoria'];
+ $txt_ativo = $_POST['txt_ativo'];
+
+ //$sql = "INSERT INTO categoria (categoria, cat_ativo) VALUES ('$txt_categoria','$txt_ativo')";
+ //msqli_query($db, $sql) or die("Não foi possível inserir dados!");
+ // $cn = new PDO("mysql:host=localhost;dbname=dinamicodb","root", "usbw");
+ //$cn->setAttribute(PDO::ATTR_ERRMODE, PDO_EXCEPTION);
+    try { 
+        $sql = "INSERT INTO categoria (categoria, cat_ativo) VALUES ('$txt_categoria','$txt_ativo')";
+        $cn->prepare($sql)->execute([$txt_categoria, $txt_ativo]);
+        }catch(exception $e){
+       
+        echo "Error", $e->getMessage();
+       }
+   
+?>
